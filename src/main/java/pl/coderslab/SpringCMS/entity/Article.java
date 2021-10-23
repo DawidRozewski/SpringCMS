62ca72fa6b179c136782f212849520d62e795966
DawidRozewski <dawid.rrozewski@gmail.com>
2021-10-23 20:28:30 +0200

    Zadanie 3.

---
+++ src/main/java/pl/coderslab/SpringCMS/entity/Article.java
@@ -0,0 +1,34 @@
+package pl.coderslab.SpringCMS.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Transactional
+@Getter
+@Setter
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(length = 200)
+    private String title;
+
+    @OneToOne
+    private Author author;
+
+    @OneToMany
+    private List<Category> categories = new ArrayList<>();
+
+    private LocalDateTime created;
+    private LocalDateTime updated;
+
+
+}
