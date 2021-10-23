188600692c429be1d4a7d140bdb41dd37a308738
DawidRozewski <dawid.rrozewski@gmail.com>
2021-10-23 20:24:42 +0200

    Zadanie 2.

--- src/main/java/pl/coderslab/SpringCMS/entity/Author.java
+++ src/main/java/pl/coderslab/SpringCMS/entity/Author.java
@@ -1,20 +1,22 @@
 package pl.coderslab.SpringCMS.entity;
+
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.lang.Nullable;
-import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.transaction.Transactional;
 
 @Entity
 @Transactional
 @Getter
 @Setter
-public class Category {
+public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String name;
-    @Nullable
-    private String description;
-
+    private String firstName;
+    private String lastName;
 }
