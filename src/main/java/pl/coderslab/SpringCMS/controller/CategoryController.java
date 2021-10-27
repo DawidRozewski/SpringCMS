42a0f335b00dfe9b274b7ca3a5148f23aa0d5974
DawidRozewski <dawid.rrozewski@gmail.com>
2021-10-27 17:53:23 +0200

    Zadanie 3. Dzien 2.

--- src/main/java/pl/coderslab/SpringCMS/controller/CategoryController.java
+++ src/main/java/pl/coderslab/SpringCMS/controller/CategoryController.java
@@ -3,6 +3,8 @@ package pl.coderslab.SpringCMS.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.SpringCMS.dao.CategoryDao;
 import pl.coderslab.SpringCMS.entity.Category;
@@ -23,6 +25,18 @@ public class CategoryController {
     public String getAllCategories(Model model){
         model.addAttribute("categories", categoryDao.findAll());
         return "/category/showAll";
+    }
+
+    @GetMapping("/add")
+    public String showAddForm(Model model) {
+        model.addAttribute("category", new Category());
+        return "/category/addForm";
+    }
+
+    @PostMapping("/add")
+    public String saveCategory(@ModelAttribute("category") Category category) {
+        categoryDao.persist(category);
+        return "redirect:/category/all";
     }
 
 
---
+++ src/main/webapp/WEB-INF/views/category/addForm.jsp
@@ -0,0 +1,3 @@
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ page contentType="text/html; charset=UTF-8" %>
+<h2>Add new category</h2>
